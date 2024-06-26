f43809cda58e6f9d3b8f40eb8c64f1a7663a23d5
shravani mamidwar <127090635+EsEmm04@users.noreply.github.com>
2024-06-26 17:28:00 +0530

    Create test.sv

---
+++ Verification of UART/test.sv
@@ -0,0 +1,13 @@
+`include "environment.sv"
+
+program test_main(uart_intf intf);
+    
+environment env;
+
+initial begin
+    env=new(intf);
+    env.gen.no_of_tr=1000;
+    env.run();
+end
+
+endprogram
